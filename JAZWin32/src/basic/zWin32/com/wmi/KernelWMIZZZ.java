e4780c26b541c6afe65402f7f7ebdec170664545
firak01 <firak01@t-online.de>
2022-07-21 10:01:18 +0200

    IFlagLocal eingebunden
    Signed-off-by: firak01 <firak01@t-online.de>

--- JAZWin32/src/basic/zWin32/com/wmi/KernelWMIZZZ.java
+++ JAZWin32/src/basic/zWin32/com/wmi/KernelWMIZZZ.java
@@ -11,6 +11,7 @@ import basic.zBasic.util.datatype.string.StringZZZ;
 import basic.zKernel.IKernelZZZ;
 import basic.zKernel.KernelUseObjectZZZ;
 import basic.zKernel.KernelZZZ;
+import basic.zKernel.flag.IFlagUserZZZ;
 
 public class KernelWMIZZZ extends KernelUseObjectZZZ{
 	private ActiveXComponent objWMIService = null;
@@ -30,7 +31,7 @@ public class KernelWMIZZZ extends KernelUseObjectZZZ{
 				stemp = saFlagControl[iCount];
 				btemp = setFlag(stemp, true);
 				if(btemp==false){ 								   
-					   ExceptionZZZ ez = new ExceptionZZZ( stemp, iERROR_FLAG_UNAVAILABLE, this, ReflectCodeZZZ.getMethodCurrentName()); 					 
+					   ExceptionZZZ ez = new ExceptionZZZ( stemp, IFlagUserZZZ.iERROR_FLAG_UNAVAILABLE, this, ReflectCodeZZZ.getMethodCurrentName()); 					 
 					   throw ez;		 
 				}
 			}
